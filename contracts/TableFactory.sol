d373b5847d8562c97513b8b8eeb810805123d7d6
Federico Burman <fburman@fi.uba.ar>
2021-11-11 15:24:16 -0300

    agrego proyecto truffle

--- contracts/TableFactory.sol
+++ contracts/TableFactory.sol
@@ -1,38 +1,37 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
+// // SPDX-License-Identifier: GPL-3.0
+// pragma solidity ^0.8.0;
+// pragma experimental ABIEncoderV2;
 
-// hay que ver que es esto
-// import "@openzeppelin/contracts/access/Ownable.sol";
+// // hay que ver que es esto
+// // import "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract MesaFactory is Ownable {
+// // contract MesaFactory is Ownable {
 
-contract TableFactory {
+// contract TableFactory {
+//     event NewTable(uint256 id, uint256 randomNumber, uint8 maxPlayers);
 
-    event NewTable(uint id, uint randomNumber, uint8 maxPlayers);
+//     struct Table {
+//         uint256 tableId;
+//         uint8 maxPlayers;
+//         uint256 number;
+//     }
 
-    struct Table {
-        uint tableId;
-        uint8 maxPlayers;
-        uint number;
-    }
-    
-    Table[] public tables;
+//     Table[] public tables;
 
-    /*
-    mapping (uint => address) public cartaAPersona;
-    mapping (address => uint) personaCantidadCartas;
-    */
+//     /*
+//     mapping (uint => address) public cartaAPersona;
+//     mapping (uint => Table) public tables;
+//     mapping (address => uint) personaCantidadCartas;
+//     */
 
-    function _createTable(uint randomNumber, uint8 maxPlayers) internal {
-        uint id = tables.length - 1;
-        
-        tables.push(Table(id, maxPlayers, randomNumber));
-        
-        /*cartaAPersona[id] = msg.sender;
-        personaCantidadCartas[msg.sender] ++;*/
-        
-        emit NewTable(id, randomNumber, maxPlayers);
-    }
+//     function _createTable(uint256 randomNumber, uint8 maxPlayers) internal {
+//         uint256 id = tables.length;
 
-}
+//         tables.push(Table(id, maxPlayers, randomNumber));
+
+//         /*cartaAPersona[id] = msg.sender;
+//         personaCantidadCartas[msg.sender] ++;*/
+
+//         emit NewTable(id, randomNumber, maxPlayers);
+//     }
+// }
