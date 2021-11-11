ad93d88ac04d13b8c22af97df9a7bc6385d4d108
LeviMatias <matilevi35@gmail.com>
2021-11-11 12:51:00 -0300

    erc165 contract

--- NFT/ERC165.sol
+++ NFT/ERC165.sol
@@ -4,3 +4,9 @@ pragma solidity ^0.8.0;
 interface IERC165{
     function supportsInterface(bytes4 interfaceId) external view returns(bool);
 }
+
+contract ERC165 is IERC165{
+    function supportsInterface(bytes4 interfaceId) external pure override returns(bool){
+        return interfaceId == type(IERC165).interfaceId;
+    }
+}
