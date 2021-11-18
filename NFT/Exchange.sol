7fdd330cb5a542110dbca1781384804ba99f477a
LeviMatias <matilevi35@gmail.com>
2021-11-18 17:09:24 -0300

    added operator attribute as msg.sender

--- NFT/Exchange.sol
+++ NFT/Exchange.sol
@@ -5,16 +5,18 @@ import "./Token721.sol";
 
 contract Exchange{
     Token721 nfts;
+    address operator;
 
     //address must be the address where the 721Token is deployed
     constructor(address addr){
         nfts = Token721(addr);
+        operator = msg.sender;
     }
 
     // for this function to work, the address where the exchange is deployed must have been approved
     // by the token owners
-    //TODO decide who is allowed to call this function
     function exchange_between(address p1, uint256 t1, address p2, uint256 t2) public{
+        require(msg.sender == operator, "Only the operator can initiate exchanges");
         nfts.safeTransferFrom(p1, p2, t1);
         nfts.safeTransferFrom(p2, p1, t2);
     }
