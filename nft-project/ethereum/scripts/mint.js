9d6932d5464107621267440726f944bcb97db24a
Federico Burman <fburman@fi.uba.ar>
2021-12-03 15:23:50 -0300

    nft deployed con imagen

--- nft-project/ethereum/scripts/mint.js
+++ nft-project/ethereum/scripts/mint.js
@@ -7,14 +7,14 @@ const contractInterface = contract.abi;
 // https://docs.ethers.io/v5/api/providers
 const provider = ethers.getDefaultProvider("ropsten", {
     alchemy: process.env.DEV_API_URL,
-});
+}); 7
 
 // https://docs.ethers.io/v5/api/signer/#Wallet
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 //https://docs.ethers.io/v5/api/contract/contract
 const emotionalShapes = new ethers.Contract(
-    '0x21Eb356E49289C5CD56ffF65803d5A0D51067487',
+    '0xB97Cf7AC345aE4786Dc87eB486A4E9C4d30dC8d6',
     contractInterface,
     wallet
 );
