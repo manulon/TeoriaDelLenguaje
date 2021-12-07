51f5075529a001581cfc42a59d9fc08f707aad15
Azul Zaietz <azulzaietz@gmail.com>
2021-12-07 19:33:34 -0300

    + exchange

---
+++ nft-project/webb/webapp/pages/api/exchange.js
@@ -0,0 +1,32 @@
+const { ethers } = require("ethers");
+
+const exchangeContract = require("../../../../ethereum/artifacts/contracts/Exchange.sol/Exchange.json");
+const exchangeContractInterface = exchangeContract.abi;
+
+export default function handler(req, res) {
+  const provider = ethers.getDefaultProvider("ropsten", {
+    alchemy: "https://eth-ropsten.alchemyapi.io/v2/wQy1KM82GKO4lQZhn89KAUISheuDlpIi",
+  });
+  const wallet = new ethers.Wallet(req.query.key, provider);
+  const exchange = new ethers.Contract(
+    req.query.contract_addr,
+    exchangeContractInterface,
+    wallet
+  );
+
+  switch (req.method) {
+
+    case "POST":
+      exchange.exchange_between(req.query.sendTo, req.query.tokenId)
+        .then((transaction) => res.status(200).json({ "transaction": transaction }))
+        .catch((e) => res.status(405).json({ "error": e }));
+      break;
+      
+    default:
+      res.status(405).json({
+        mensaje: `El método HTTP ${req.method} no esta disponible en esta ruta`,
+      });
+      break;
+  }
+
+}
